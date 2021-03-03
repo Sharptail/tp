da782a3fab5d458a4baa00f47a269d214fcd3c0e
Sharptail <kongjianwei@outlook.com>
2021-03-03 18:34:18 +0800

    Update code style

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,11 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+    * Constructs a {@code Remark}.
+    *
+    * @param remark string to be remarked.
+    */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
